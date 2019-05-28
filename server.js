52c008eaeab486ee6eec1b3d6efe04233d318f15
Bruno <bruno@MacBook-Air.local>
2019-05-28 20:41:54 -0300

    connecting database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-closed-94727',
     user : 'bruno',
     password : '',
     database : 'frb'
